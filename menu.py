066732a1115b33d854b2f7a45020eccc4f595cf9
Rogelio G. <rogelioguerreo14@gmail.com>
2025-10-21 08:20:10 -0600

    Fixed menu

--- menu.py
+++ menu.py
@@ -8,13 +8,13 @@ class Menu:
         self.back = pygame.Surface(self.size)
         self.lenght = len(functions) 
         self.buttons = []
-
-        offset = 0
+        
+        offset = 5
         for function in functions:
-            button_size = (self.size[0] // self.lenght - 10, self.size[1] - 10)
-            button_position = (offset + (button_size[0] // 2) + 5, self.size[1] // 2 + 5)
+            button_size = (self.size[0] - 10, ( self.size[1] - 5 ) // self.lenght - 5)
+            button_position = (self.size[0] // 2, offset + button_size[1] // 2)
             button = Button(button_size, button_position, function)
-            offset +=  button_size[0]
+            offset +=  button_size[1] + 5
             self.buttons.append(button)
  
 
@@ -23,9 +23,19 @@ class Menu:
         for button in self.buttons:
             button.draw(self.back, mouse)
         
-        back_rect = self.back.get_rect(topleft = position)
-        container.blit(self.back, back_rect)
+        if position[0] + self.size[0] <= container.get_width():
+            if position[1] + self.size[1] <= container.get_height():
+                back_rect = self.back.get_rect(topleft = position)
+            else:
+                back_rect = self.back.get_rect(bottomleft = position)
+        else:
+            if position[1] + self.size[1] <= container.get_height():
+                back_rect = self.back.get_rect(topright = position)
+            else:
+                back_rect = self.back.get_rect(bottomright = position)
 
+        container.blit(self.back, back_rect)
+        
 
     def click(self, mouse: tuple[int], clicked: tuple[int]):
         
